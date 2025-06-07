eec29b1df189ea6df6fa926d203f3b5bc60003bd
Ferds <ferdreyes20@gmail.com>
2025-06-08 04:48:36 +0800

    S4: Create login return jwtToken

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,12 +3,17 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class AccountController(DataContext context) : BaseApiController
+    public class AccountController(
+        DataContext context,
+        ITokenService tokenService
+    ) : BaseApiController
     {
         [HttpPost("register")]
         public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
@@ -30,6 +35,30 @@ namespace API.Controllers
             return Ok(user);
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(x => x.Username == loginDto.Username.ToLower());
+
+            if (user == null) return Unauthorized("Invalid username");
+            
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            var userDto = new UserDto
+            {
+                Username = user.Username,
+                Token = tokenService.CreateToken(user)
+            };
+
+            return Ok(userDto);
+        }
         private async Task<bool> UserExists(string username)
         {
             return await context.Users.AnyAsync(x => x.Username.ToLower() == username.ToLower());
