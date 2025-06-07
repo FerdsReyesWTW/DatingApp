a7e59b44388f4f41db029e8a3bec0d9c15e1c61d
Ferds <ferdreyes20@gmail.com>
2025-06-08 02:55:17 +0800

    S3: Added http get Users

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'DatingApp';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: res => this.users = res,
+      error: err => console.log(err),
+      complete: () => console.log('request completed')
+    });
+  }
 }
