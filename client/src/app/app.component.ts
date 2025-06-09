4601674befc3af6a310e007d3c44ef675c6fdc61
Ferds <ferdreyes20@gmail.com>
2025-06-09 09:42:56 +0800

    S5: Added Register form

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [NavComponent]
+  imports: [NavComponent, HomeComponent]
 })
 export class AppComponent implements OnInit {
   private http = inject(HttpClient);
