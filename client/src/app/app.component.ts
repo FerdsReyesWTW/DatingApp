9711a91280f9aa7466ec9f9af66822001f9aaf17
Ferds <ferdreyes20@gmail.com>
2025-06-09 09:13:08 +0800

    S5: FE Added  login and login function

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,20 +1,34 @@
-import { NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
+import { NavComponent } from './nav/nav.component';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [NavComponent]
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
+  private http = inject(HttpClient);
+  private accountService = inject(AccountService);
   title = 'DatingApp';
   users: any;
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: res => this.users = res,
       error: err => console.log(err),
